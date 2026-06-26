02964cec747c0fc574904c4f2e8124ac3cd0760a
bortolitl-git <tiago.bortoli@autoinsp.com>
2026-06-26 08:14:59 -0300

    Remove assetPrefix configuration

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,7 @@ const nextConfig = {
     remotePatterns: prepareRemotePatterns(),
   },
   skipTrailingSlashRedirect: true,
-  assetPrefix:
-    process.env.NODE_ENV === "production" &&
-    process.env.VERCEL_ENV === "production"
-      ? process.env.NEXT_PUBLIC_BASE_URL
-      : undefined,
+  assetPrefix: undefined,
   async redirects() {
     return [
       {
